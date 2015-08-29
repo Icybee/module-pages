7757f13ff9cda52e7812be945487da8c248d836d
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-29 20:20:57 +0200

    Added default 'home' template

---
+++ templates/home.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brickrouge;
+
+/**
+ * @var $this \Icybee\Modules\Pages\Page
+ * @var $page \Icybee\Modules\Pages\Page
+ * @var $document \Icybee\Document
+ * @var $tried_templates array
+ */
+
+$current_template = array_pop($tried_templates);
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= escape($this->title) ?></title>
+
+	<?= $document->css ?>
+</head>
+<body>
+
+<div class="container">
+	<p>THIS IS A SAMPLE TEMPLATE</p>
+
+	<p>This view was rendered using the following template:</p>
+
+	<p><code><?= $current_template ?></code></p>
+
+	<p>You may override this by creating one of the following template:</p>
+
+	<ul>
+		<?php foreach($tried_templates as $template): ?>
+		<li><code><?= $template ?></code></li>
+		<?php endforeach; ?>
+	</ul>
+</div>
+
+<?= $document->js ?>
+
+</body>
