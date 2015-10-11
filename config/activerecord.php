6a918cf088c42f156ac2b7e0ad021978f4197d61
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-11 18:23:32 +0200

    Added facets

---
+++ config/activerecord.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icybee\Modules\Pages;
+
+use ICanBoogie\Facets\BooleanCriterion;
+
+return [
+
+	'facets' => [
+
+		'pages' => [
+
+			'is_navigation_excluded' => BooleanCriterion::class
+
+		]
+
+	]
+
+];
